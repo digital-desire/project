5d39a74a0f9632641b60566d914bdfd0580d3274
wojtiwojt <wojti.wojt91@gmail.com>
2022-03-08 19:39:18 +0100

    Update API

--- django/pozabanka/api/viewsets.py
+++ django/pozabanka/api/viewsets.py
@@ -2,13 +2,12 @@ from django.shortcuts import get_object_or_404
 from pozabanka.api.serializers import ArticleSerializer, SourceSerializer
 from pozabanka.articles.models import Article, Source
 from rest_framework import status, viewsets
-from rest_framework.decorators import action
 from rest_framework.response import Response
 
 
 class ArticleViewSet(viewsets.ViewSet):
 
-    # TODO: permissions, methods for update
+    # TODO: permissions??
 
     def list(self, request):
         queryset = Article.objects.all()
@@ -31,14 +30,53 @@ class ArticleViewSet(viewsets.ViewSet):
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    @action(detail=True, methods=["post"])
-    def add_source(self, request, pk=None):
-        user = request.user
+    def update(self, request, pk=None, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
         article = get_object_or_404(Article, pk=pk)
-        serializer = SourceSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        Source.objects.create(author=user, article=article, **serializer.validated_data)
-        return Response(
-            ArticleSerializer(article, context=request).data,
-            status=status.HTTP_201_CREATED,
+        serializer = ArticleSerializer(
+            article, data=request.data, context=request, partial=partial
         )
+        serializer.is_valid(raise_exception=True)
+        serializer.update(article, serializer.validated_data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def partial_update(self, request, pk=None, *args, **kwargs):
+        kwargs["partial"] = True
+        return self.update(request, pk=pk, *args, **kwargs)
+
+
+class SourceViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Source.objects.all()
+        serializer = SourceSerializer(queryset, many=True, context=request)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        source = get_object_or_404(Source, pk=pk)
+        serializer = SourceSerializer(source, context=request)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = SourceSerializer(data=request.data, context=request)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def destroy(self, request, pk=None):
+        source = get_object_or_404(Source, pk=pk)
+        source.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def update(self, request, pk=None, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        source = get_object_or_404(Source, pk=pk)
+        serializer = SourceSerializer(
+            source, data=request.data, context=request, partial=partial
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.update(source, serializer.validated_data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def partial_update(self, request, pk=None, *args, **kwargs):
+        kwargs["partial"] = True
+        return self.update(request, pk=pk, *args, **kwargs)
